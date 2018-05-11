d396a42eab41879997e64d78aea40b9802a1c60b
glydrfreak <braedinbutler@gmail.com>
2018-05-11 15:53:18 -0600

    options to change units

--- vSpeed_mini/DEFAULT_SETTINGS.h
+++ vSpeed_mini/DEFAULT_SETTINGS.h
@@ -27,6 +27,8 @@ class Default{
     bool ENABLE_BLUETOOTH            = 1;    // [1]YES, [0]NO
     int START_UP_BLUETOOTH_MODE      = 0;    // [0]DISABLE, [1]VSPEED_ANDROID, [2]FLYSKYHY_IOS
 
+    int ALTITUDE_UNITS               = 1;    // [1]FEET, [2]METERS
+    int VELOCITY_UNITS               = 1;    // [1]FEET_PER_SECOND, [2]METERS_PER_SECOND, [3]FEET_PER_MINUTE
 };
     
 #endif
